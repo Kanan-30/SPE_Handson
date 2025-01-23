cdf916fe5cdd0bea23cad6ba2471844a6e370bb9
Kanan Gupta <85862474+Kanan-30@users.noreply.github.com>
2025-01-23 10:52:04 +0530

    Create calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,24 @@
+# !/bin/bash
+a=10
+b=20
+
+echo "ENter choice: "
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+
+ch=1
+
+case $ch in
+        1)res=`expr $a + $b`
+                ;;
+        2)res=`expr $b - $a`
+                ;;
+        3)res=`expr $a \* $b`
+                ;;
+        4)res=`expr $a / $b`
+                ;;
+esac
+echo "Result: $res"
+
